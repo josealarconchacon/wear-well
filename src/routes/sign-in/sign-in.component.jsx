4678522930417bd3c67f47e8219666d90cdc9a25
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-09-01 12:59:21 -0400

    setting up firebase db

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+  };
   return (
     <div>
-      <h1>SignIn</h1>
+      <h1>SignIn Page</h1>
+      <button onClick={logGoogleUser}>Sign In with Google</button>
     </div>
   );
 };
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,56 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  Firestore,
+} from "firebase/firestore";
+
+// firebase config
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "wear-well-db.firebaseapp.com",
+  projectId: "wear-well-db",
+  storageBucket: "wear-well-db.appspot.com",
+  messagingSenderId: "929980133346",
+  appId: "1:929980133346:web:b40ec6e3e411be754053bb",
+};
+
+// Initialize Firebase
+const fbApplication = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating user document", error);
+    }
+  }
+};
