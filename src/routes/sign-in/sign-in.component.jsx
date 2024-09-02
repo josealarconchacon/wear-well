e67b4e7d45c281e1bf8cbcd4e717b6eb59c4d0fc
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-09-01 20:13:33 -0400

    logGoogleUser

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,23 @@
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase.utils";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
+    try {
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user);
+    } catch (error) {
+      console.error("Error logging in with Google:", error);
+    }
   };
+
   return (
     <div>
-      <h1>SignIn Page</h1>
+      <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google</button>
     </div>
   );
